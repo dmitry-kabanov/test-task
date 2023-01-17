d15c5d333aa2e08d3dfaa0ba8abf0a70d01032e0
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2023-01-17 15:58:13 +0100

    Test convex case for Python callbacks

--- tests/optimize/test_steepest_descent.py
+++ tests/optimize/test_steepest_descent.py
@@ -4,13 +4,37 @@ import numpy.testing as npt
 from optimize import SteepestDescent
 
 
-def test_1():
+def convex_function(xstar=[1.0, 2.0]):
     def f(x):
-        return (x[0] - 1) ** 2 + (x[1] - 2) ** 2
+        return np.sum(np.square(x - xstar))
 
-    x = np.array([3.0, 3.0])
-    gd = SteepestDescent()
-    xmin = gd.minimize(f, x)
-    desired = np.array([1.0, 2.0])
+    return f
 
-    npt.assert_allclose(xmin, desired, rtol=1e-4, atol=1e-8)
+
+def test_steepest_descent__2d_convex_case__minimizes_well():
+    x0 = np.array([3.0, 3.0])
+    xstar = np.array([1.0, 2.0])
+
+    xmin = SteepestDescent().minimize(convex_function(xstar), x0)
+
+    npt.assert_allclose(xmin, xstar, rtol=1e-4, atol=1e-8)
+
+
+def test_steepest_descent__2d_convex_case__one_step_decreases_function_value():
+    x0 = np.random.random(size=(2,))
+    f = convex_function()
+
+    x1 = SteepestDescent(max_iter=1).minimize(f, x0)
+
+    assert f(x1) < f(x0)
+
+
+def test_steepest_descent__ND_convex_case__minimizes_well():
+    N = 5
+    xstar = np.random.normal(loc=np.arange(N), scale=7, size=(N,))
+    x0 = np.random.normal(loc=0.0, scale=3, size=(N,))
+    f = convex_function(xstar)
+
+    xmin = SteepestDescent().minimize(f, x0)
+
+    npt.assert_allclose(xmin, xstar, rtol=1e-4, atol=1e-8)

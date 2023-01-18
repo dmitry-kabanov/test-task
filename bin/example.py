7f8e44cd5ec2d66f4978d20649842f3334400136
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2023-01-18 22:06:54 +0100

    Add docstrings to SteepestDescent class

--- bin/example.py
+++ bin/example.py
@@ -3,17 +3,12 @@ from optimize.functionals import parabola_nd
 
 
 def main():
-    opt = SteepestDescent(verbose=False)
+    opt = SteepestDescent(step_size=1e-1, tol=1e-6, verbose=True)
     x0 = (2.0, 1.0, 5.0)
     xmin = opt.minimize(parabola_nd, x0)
 
     print(f"xmin: {xmin}")
-    if opt.reason == opt.CONVERGENCE:
-        print("Reason of optimization end: convergence")
-    elif opt.reason == opt.MAX_ITERATIONS:
-        print("Reason of optimization end: exceed number of max iterations")
-    else:
-        raise Exception("Unknown optimization error")
+    assert opt.status == opt.CONVERGENCE
 
 
 if __name__ == "__main__":
